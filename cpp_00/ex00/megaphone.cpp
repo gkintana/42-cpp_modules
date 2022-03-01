a20fd12f10e4056b303ca25e62fdcbaf92a9e1d7
gkintana <81775867+gkintana@users.noreply.github.com>
2022-03-01 17:38:07 +0400

    megaphone refactor

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,32 +6,25 @@
 /*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 20:59:04 by gkintana          #+#    #+#             */
-/*   Updated: 2022/02/27 23:48:34 by gkintana         ###   ########.fr       */
+/*   Updated: 2022/03/01 17:36:58 by gkintana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-char	ft_toupper(char c)
-{
+char	ft_toupper(char c) {
 	if (c >= 97 && c <= 122)
 		return (c - 32);
 	return (c);
 }
 
-int	main(int argc, char **argv)
-{
-	int	i;
-	int	j;
-
+int	main(int argc, char **argv) {
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		i = 0;
-		while (++i < argc)
-		{
-			j = 0;
+	else {
+		int	i = 0;
+		while (++i < argc) {
+			int	j = 0;
 			while (argv[i][j])
 				std::cout << ft_toupper(argv[i][j++]);
 		}
