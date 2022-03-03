9ea2fe042c2e20bd82019a605da42a562b5120de
gkintana <81775867+gkintana@users.noreply.github.com>
2022-03-03 22:14:43 +0400

    add cpp01 ex02

---
+++ cpp_01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 20:09:17 by gkintana          #+#    #+#             */
+/*   Updated: 2022/03/03 22:11:10 by gkintana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void) {
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*strPtr = &str;
+	std::string	&strRef = str;
+
+	std::cout << "Memory Addresses:" << std::endl;
+	std::cout << "  • String Variable\t= " << &str << std::endl;
+	std::cout << "  • String Pointer\t= " << strPtr << std::endl;
+	std::cout << "  • String Reference\t= " << &strRef << std::endl;
+	std::cout << "String Values:" << std::endl;
+	std::cout << "  • String Variable\t= " << str << std::endl;
+	std::cout << "  • String Pointer\t= " << *strPtr << std::endl;
+	std::cout << "  • String Reference\t= " << strRef << std::endl;
+	return (0);
+}
