38c20d59d81c20197c1ba85aa4d74ce8570912c8
gkintana <81775867+gkintana@users.noreply.github.com>
2022-06-14 00:40:52 +0400

    refactor cpp05 ex02

--- cpp_05/ex02/ShrubberyCreationForm.hpp
+++ cpp_05/ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,7 @@
 /*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 22:14:16 by gkintana          #+#    #+#             */
-/*   Updated: 2022/06/13 19:10:09 by gkintana         ###   ########.fr       */
+/*   Updated: 2022/06/14 00:28:30 by gkintana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@
 class ShrubberyCreationForm : public Form {
 	private:
 		std::string const m_target;
+		void createTarget() const;
 	public:
 		ShrubberyCreationForm();
 		ShrubberyCreationForm(std::string const target);
